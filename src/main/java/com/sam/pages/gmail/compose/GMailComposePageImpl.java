92b49bb491bcc94a63d8d9f32b6a55bbad6753a0
Volga Haliankevich <olgagol@sam-solutions.net>
2019-08-19 11:56:52 +0300

    fix split packages

--- src/main/java/com/sam/pages/gmail/compose/GMailComposePageImpl.java
+++ src/main/java/com/sam/pages/gmail/compose/GMailComposePageImpl.java
@@ -3,30 +3,43 @@ package com.sam.pages.gmail.compose;
 import com.sam.pages.PageImpl;
 import com.sam.pages.base.compose.Compose;
 import com.sam.pages.gmail.main.GMailMainPage;
+import com.sam.pages.gmail.main.GMailMainPageImpl;
 
-public class GMailComposePageImpl extends PageImpl<Compose> implements GMailComposePage{
+public class GMailComposePageImpl extends PageImpl<Compose> implements GMailComposePage {
 
     public GMailComposePageImpl() {
-        super( new GMailComposeImpl());
+        super(new GMailComposeImpl());
     }
 
     @Override
     public GMailComposePage writeLetter(String recipient, String subject, String letter) {
-        return null;
+        content.setRecipient(recipient);
+        content.setSubject(subject);
+        content.writeLetter(letter);
+        return this;
     }
 
     @Override
     public GMailComposePage writeLetter(String to, String topic, String letterBody, String letterStart, String letterEnd) {
-        return null;
+        content.setRecipient(to);
+        content.setSubject(topic);
+        content.writeLetter(letterStart, letterBody, letterEnd);
+        return this;
     }
 
     @Override
     public GMailMainPage sendLetter() {
-        return null;
+        content.sendLetter();
+        if (AlertAbsentRecipient.exists()) {
+            AlertAbsentRecipient.close();
+        }
+        return new GMailMainPageImpl();
     }
 
     @Override
     public GMailMainPage closePage() {
-        return null;
+         content.close();
+        return new GMailMainPageImpl();
     }
+
 }
